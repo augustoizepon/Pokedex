adb0e3fd65f1bbf0f3a05739e375a730430b1baf
augusto.izepon <augustom.izepon@gmail.com>
2023-08-22 02:30:52 -0300

    limitando para os pokemons classicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=648`;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=150`;
 const content = document.querySelector(".content");
 const olElement = document.querySelector(".pokemons");
 
